89028d0621cba65075aeee87b647dc066154e18f
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2015-10-18 19:34:46 +0800

    Fixed bug on deleting resident

--- ajax/delete_resident.php
+++ ajax/delete_resident.php
@@ -2,6 +2,6 @@
 require "../lib/db.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db = new DbManager();
-    $db->execute("DELETE FROM residents WHERE resident_id = ?;", array($_POST['id']));
+    $db->execute("DELETE FROM residents WHERE id = ?;", array($_POST['id']));
     $db->execute("DELETE FROM owner_residents WHERE resident_id = ?;", array($_POST['id']));
 }
